13c3b16b067e040d86b342e9b83cdcf348258fb9
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-05-02 11:24:57 -0600

    Updated the Weather.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ function App() {
       <header className="App-header">
         <h1> Weather React</h1>
         <Weather />
+        <small>
+          <a
+            rel="noreferrer"
+            href="https://github.com/olayemiajayi/weather-react-app"
+            target="_blank"
+          >
+            {" "}
+            Open-Source code
+          </a>{" "}
+          By Yetunde Ajayi
+        </small>
       </header>
     </div>
   );
